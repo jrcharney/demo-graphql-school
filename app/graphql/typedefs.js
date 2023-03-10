829056f004b1299649b57c4e6ce4981a142fb417
Jason Charney <jrcharney@gmail.com>
2023-03-09 20:49:36 -0600

    Added graphql-tag

---
+++ app/graphql/typedefs.js
@@ -0,0 +1,23 @@
+import { gql } from "graphql-tag";
+
+// Lines ending with `!` mean "require/not null"
+// `_id` is MongoDB id
+const typeDefs = gql`
+  type Query {
+    "Get all the classes. If there are no classes, return an empty array."
+    indexTracks: [Class]!
+  }
+  "A class in our school."
+  type Class {
+    "The unique identifier for the class. This is a MongoDB ID."
+    id: ID!
+    "Name of the class."
+    name: String!
+    "In what classroom and building is the class."
+    building: String!
+    "Number of credit hours the class is worth."
+    creditHours: Int!
+  }
+`;
+
+export default typeDefs;
